7322d13582519c2363a990788742892fc8563c84
Adegbuyi Ademola <marhyorh@gmail.com>
2015-11-13 22:40:18 +0100

    Added GitHub Oauth

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
