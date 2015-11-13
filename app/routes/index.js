ebbf4cfe9b904b837ae6bd6f9c30976fc1bc2bc2
Adegbuyi Ademola <marhyorh@gmail.com>
2015-11-13 23:37:08 +0100

    Added GitHub Oauth

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,21 +17,21 @@ module.exports = function (app, passport) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+//	app.route('/login')
+//		.get(isLoggedIn, function (req, res) {
+//			res.sendFile(path + '/public/login.html');
+//		});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+//	app.route('/logout')
+//		.get(function (req, res) {
+//			req.logout();
+//			res.redirect('/login');
+//		});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+//	app.route('/profile')
+//		.get(isLoggedIn, function (req, res) {
+//			res.sendFile(path + '/public/profile.html');
+//		});
 
 	app.route('/api/:id')
 		.get(isLoggedIn, function (req, res) {
